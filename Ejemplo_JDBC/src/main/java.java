2188f9e860b77c6a4193885481d16cbdacb2c89e
Jose Moises Granados <jgranados.iastic@gmail.com>
2017-08-03 23:40:08 -0600

    Coneccion basica JDBC

---
+++ Ejemplo_JDBC/src/main/java.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import java.sql.Array;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ *
+ * @author jose
+ */
+public class java {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		try {
+			//Indicamos cual driver usar
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			
+			//abrimos una coneccion a la DB usando una url en la que indicamos el
+			//nombre de la base da tos (schema) y como parametros mandamos el usuario y password (SUSTITUIR PASSWORD)
+			Connection connection = 
+				   DriverManager.getConnection("jdbc:mysql://localhost/mysql?user=root&password=ajedrez21Mysql");
+			
+			//Mostramos el nombre del esquema de base de datos, en este caso es mysql
+			System.out.println("conectado:" + connection.getCatalog());
+			//a partir de la coneccion creamos un objecto Statement para poder ejecutar codigo SQL en la DB
+			Statement declaracion = connection.createStatement();
+			
+			//ejecutamos la consulta y guardamos los datos en un objeto ResultSet
+			ResultSet resultado = declaracion.executeQuery("SELECT Db, User FROM db;");
+			
+			int numeroFila = 1;
+			System.out.println("Datos:");
+			//recorremos cada fila usando el metodo next del resultset.
+			while (resultado.next()) {
+				System.out.println("Fila " + numeroFila + "-" + "Db: " + resultado.getString("Db") + " | User: " + resultado.getString("User"));
+				numeroFila++;
+			}
+			
+			
+		} catch (Exception e) {
+			System.out.println("error");
+		}
+	}
+
+}
